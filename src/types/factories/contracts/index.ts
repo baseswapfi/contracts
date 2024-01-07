67888fa77e05ea7c244627ff25db6628c83ceba9
0xshoegazer <0xshoegazer@gmail.com>
2024-01-07 18:10:31 -0500

    nft pool factory export

--- src/types/factories/contracts/index.ts
+++ src/types/factories/contracts/index.ts
@@ -1,3 +1,4 @@
 export { MasterChef__factory } from './MasterChef__factory';
 export { NFTPool__factory } from './NFTPool__factory';
 export { ERC20__factory } from './ERC20__factory';
+export { NFTPoolFactory__factory } from './NFTPoolFactory__factory';
