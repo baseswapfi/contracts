a35e34175baa9ca27355f6ca9fe44a8ad9abe9c9
0xshoegazer <0xshoegazer@gmail.com>
2024-01-08 15:04:02 -0500

    nft pool utils

--- src/utils/nft-pool.utils.ts
+++ src/utils/nft-pool.utils.ts
@@ -1,7 +1,8 @@
 import { ChainId } from '@baseswapfi/sdk-core';
-import { NFTPoolFactory__factory } from '../types/factories/contracts';
+import { NFTPoolFactory__factory, NFTPool__factory } from '../types/factories/contracts';
 import { getBaseswapContractAddress } from './contract.utils';
 import { doTransaction } from './transaction.utils';
+import { parseUnits } from 'ethers/lib/utils';
 
 /**
  * Creates a chain specific instance the NFTPoolFactory contract
@@ -11,6 +12,20 @@ import { doTransaction } from './transaction.utils';
  */
 export function getNftPoolFactory(chainId: ChainId, signer) {
   return NFTPoolFactory__factory.connect(getBaseswapContractAddress(chainId, 'NftPoolFactory'), signer);
+}
+
+/**
+ * Gets a contract instance of an NFTPool
+ * @param nftPoolAddress
+ * @param signer
+ * @returns
+ */
+export function getNftPool(
+  nftPoolAddress: string,
+
+  signer
+) {
+  return NFTPool__factory.connect(nftPoolAddress, signer);
 }
 
 /**
@@ -25,4 +40,26 @@ export async function createNftPool(depositToken: string, chainId: ChainId, sign
   const txReceipt = await doTransaction(factory.createPool(depositToken));
   const event = txReceipt.events?.find(e => e.event === 'PoolCreated');
   return event?.args?.pool;
+}
+
+/**
+ * Creates an spNFT position for an NFTPool
+ * @param nftPoolAddress
+ * @param amount
+ * @param lockDurationSeconds
+ * @param signer
+ * @returns tokenId of the new position
+ */
+export async function createNftPoolPosition(
+  nftPoolAddress: string,
+  amount: string,
+  lockDurationSeconds: number,
+  signer
+): Promise<number> {
+  const pool = getNftPool(nftPoolAddress, signer);
+  const txReceipt = await doTransaction(pool.createPosition(parseUnits(amount), lockDurationSeconds));
+  const event = txReceipt?.events?.find(e => e.event === 'CreatePosition');
+  const tokenId = event?.args?.tokenId.toNumber();
+
+  return tokenId.toNumber();
 }
